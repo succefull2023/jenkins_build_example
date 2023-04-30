8ca70e61b2ab285bd6bf897f2d1fdda22415faab
rosine <rosinemilie@yahoo.fr>
2023-04-30 09:14:59 +0200

    1er commit

---
+++ src/main/java/com/example/jenkins_build_example/JenkinsBuildExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.jenkins_build_example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsBuildExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsBuildExampleApplication.class, args);
+	}
+
+}
